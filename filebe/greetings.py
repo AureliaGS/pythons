6dbaa6aa28546129a0d25478cef0d8876dfeaa82
ZuiMageska <130464867+ZuiMageska@users.noreply.github.com>
2023-04-11 18:24:01 +0200

    <=
    hii aurelia lets play minecraft

---
+++ filebe/greetings.py
@@ -0,0 +1,11 @@
+name = input ('What\'s your name? ')
+greeting = 'Good morning, ' + name + '!'
+print (greeting)
+a = input ('How old are you? ')
+age=int(a)
+if age >= 13 and age <= 19:
+    print ('Hey teenager!')
+else :
+    print ('your not a teen :I ' + name)
+
+
