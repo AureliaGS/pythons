072cc08feab129c4425aa836d7c197f6e066d6ed
Aurelia Sikka <86154629+AureliaGS@users.noreply.github.com>
2023-04-12 15:22:30 +0200

    eggy

---
+++ eggy.py
@@ -0,0 +1,8 @@
+print('Greetings Earthlander!')
+chickens = int (input('How many chickens do you have?'))
+eggs = int (input('How many eggs do you have?'))
+epc = eggs/chickens 
+print('Your chickens lay '+ str(epc)+' eggs on average.')
+
+
+
